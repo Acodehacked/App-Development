f212046b46ced8a9dc1460d15b7c62533cfe5669
Abin Antony <abina5448@gmail.com>
2025-06-21 23:45:26 +0530

    Vroom Companion

---
+++ Tasks/vroom-companion-task_six/test/vroom_companion_task_six_test.dart
@@ -0,0 +1,8 @@
+import 'package:vroom_companion_task_six/vroom_companion_task_six.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
