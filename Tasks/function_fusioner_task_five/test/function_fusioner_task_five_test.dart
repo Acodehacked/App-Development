5f729a2d2d8e2705e897f590206bea0d2efdf5a6
Abin Antony <abina5448@gmail.com>
2025-06-21 22:46:15 +0530

    task 5 - functiion fusioner

---
+++ Tasks/function_fusioner_task_five/test/function_fusioner_task_five_test.dart
@@ -0,0 +1,8 @@
+import 'package:function_fusioner_task_five/function_fusioner_task_five.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
