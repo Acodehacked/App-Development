e75c77735cefcc56d4c880beec401b9952a0e0b9
Abin Antony <abina5448@gmail.com>
2025-06-21 22:41:35 +0530

    task four

---
+++ Tasks/sumplier_madness_task_four/test/sumplier_madness_task_four_test.dart
@@ -0,0 +1,8 @@
+import 'package:sumplier_madness_task_four/sumplier_madness_task_four.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
